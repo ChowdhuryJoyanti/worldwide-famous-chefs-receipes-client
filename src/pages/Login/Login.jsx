1186b2af2da7173a8e2f727fb3d0d002c394cd94
Joyantichy <120155482+Joyantichy@users.noreply.github.com>
2023-05-08 12:52:19 +0600

    log in added .

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const Login = () => {
-    const handleLogin = () => {
-       
+  const {signIn} = useContext(AuthContext);
+    const handleLogin = event => {
+       event.preventDefault();
+       const from = event.target;
+      const email = from.email.value;
+      const password = from.password.value;
+      console.log(email,password);
+
+
+      signIn(email,password)
+      .then(result =>{
+          const loggedUser = result.user;
+          console.log(loggedUser);
+      })
+      .catch(error =>{
+        console.log(error);
+      })
     }
     return (
         <Container className='mx-auto w-25'>
           <h3>Please Login</h3>
-        <Form>
+        <Form onSubmit={handleLogin}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" name='email' placeholder="Enter email" required/>
